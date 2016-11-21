da59fac791db5dd211556a7d8c6983f4356b3544
Jun Yin <injune1123@gmail.com>
2016-11-21 13:27:33 -0500

    like

--- static/js/home.js
+++ static/js/home.js
@@ -9,14 +9,52 @@ var likeMainVideo = false;
 var addToggleIconListener = function(icon){
 	icon.addEventListener("click", function(){
 		likeMainVideo = !likeMainVideo;
+console.log(likeMainVideo);
+        
 	});
 }
 
 addToggleIconListener(likeMainVideoIcon,likeMainVideo );
 addToggleIconListener(dislikeMainVideoIcon, likeMainVideo);
 
+console.log(likeMainVideo);
 // send movie like to database
 
 
 
+
+//add interval
+var last_m=[],start;
+var myVideo = document.getElementById("html-main-video");
+myVideo.ontimeupdate = show;
+myVideo.onplay = play;
+myVideo.onpause= pause;
+myVideo.onseeked= seeked;
+myVideo.controls = true;
+
+function seeked(){
+        start = myVideo.currentTime;
+        myVideo.play();
+}
+function show(){
+        last_m.push(myVideo.currentTime);
+}
+function play() {
+        start = myVideo.currentTime;
+}
+function pause() {
+      
+    console.log("watched interval:"+start+":"+last_m[last_m.length-3]);
+        //upload interval here
+    last_m=[];
+}
+function makeBig() {
+    myVideo.width = 560;
+}
+function makeSmall() {
+    myVideo.width = 320;
+}
+function makeNormal() {
+    myVideo.width = 420;
+}
 
