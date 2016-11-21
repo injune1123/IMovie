f7b9b6aace60689a28ed52375a804e4cb2568e1a
Jun Yin <injune1123@gmail.com>
2016-11-21 11:45:09 -0500

    add the like button

--- static/js/home.js
+++ static/js/home.js
@@ -1,1 +1,22 @@
-// movie like
+// toggle movie like
+
+var likeMainVideoIcon = document.getElementById("html-main-video-section").getElementsByClassName('fa-heart-o')[0];
+var dislikeMainVideoIcon = document.getElementById("html-main-video-section").getElementsByClassName('fa-heart')[0];
+
+// should get it from db
+var likeMainVideo = false;
+
+var addToggleIconListener = function(icon){
+	icon.addEventListener("click", function(){
+		likeMainVideo = !likeMainVideo;
+	});
+}
+
+addToggleIconListener(likeMainVideoIcon,likeMainVideo );
+addToggleIconListener(dislikeMainVideoIcon, likeMainVideo);
+
+// send movie like to database
+
+
+
+
