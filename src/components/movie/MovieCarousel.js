9d666d455e0be5e43466c8c73225856bc527058f
mirandazhjm <Jingmei@dyn-160-39-193-210.dyn.columbia.edu>
2016-12-19 00:06:06 -0500

    background

--- src/components/movie/MovieCarousel.js
+++ src/components/movie/MovieCarousel.js
@@ -9,8 +9,46 @@ import {Carousel, Button} from 'react-bootstrap';
 class MovieCarousel extends React.Component{
 	constructor(props, context){
 		super(props, context);
-    console.log('Carousel',this.props.movies)
+    this.saveCurMovieInfo1 = this.saveCurMovieInfo1.bind(this);
+    this.saveCurMovieInfo2 = this.saveCurMovieInfo2.bind(this);
+    this.saveCurMovieInfo3 = this.saveCurMovieInfo3.bind(this);
 	}
+  saveCurMovieInfo1(){
+    sessionStorage.currentMovieId=1721;
+    sessionStorage.currentMovieLink='https://s3-us-west-1.amazonaws.com/movie-project/Titanic+Trailer+(as+a+Romantic+Comedy+movie)-aXgkYZLW25c.mp4';
+
+        var data = {};
+        data['uid'] = sessionStorage.currentUserId;
+        data['mid'] = sessionStorage.currentMovieId;
+        data['epoch'] = new Date().getTime();
+        console.log('save',data);
+        socket.emit('click_video', data);
+  }
+
+    saveCurMovieInfo2(){
+    sessionStorage.currentMovieId=101362;
+    sessionStorage.currentMovieLink='https://s3.amazonaws.com/actionmovie/Olympus+Has+Fallen+-+Official+Trailer+(HD)-vwx1f0kyNwI.mp4';
+
+        var data = {};
+        data['uid'] = sessionStorage.currentUserId;
+        data['mid'] = sessionStorage.currentMovieId;
+        data['epoch'] = new Date().getTime();
+        console.log('save',data);
+        socket.emit('click_video', data);
+  }
+
+    saveCurMovieInfo3(){
+    sessionStorage.currentMovieId=4223;
+    sessionStorage.currentMovieLink='https://s3.amazonaws.com/warmovie/Enemy+at+the+Gates+(2001)+Official+Trailer+%231+-+Jude+Law+Movie+HD-4O-sMh_DO6I.mp4';
+
+        var data = {};
+        data['uid'] = sessionStorage.currentUserId;
+        data['mid'] = sessionStorage.currentMovieId;
+        data['epoch'] = new Date().getTime();
+        console.log('save',data);
+        socket.emit('click_video', data);
+  }
+
 	render(){
 		return (  <Carousel>
 
@@ -19,9 +57,9 @@ class MovieCarousel extends React.Component{
       <img width={900} height={500} alt="900x500" src="http://cdn.history.com/sites/2/2015/05/hith-titanic-tombstone-E.jpeg"/>
       <Carousel.Caption>
         <Link to="movie">
-          <Button bsStyle="info">Watch Now</Button>
+          <Button bsStyle="info"onClick={this.saveCurMovieInfo1}>Watch Now</Button>
         </Link>
-        <h3>First slide label</h3>
+        <h3>Titanic (1997)</h3>
         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
       </Carousel.Caption>
     </Carousel.Item>
@@ -30,12 +68,12 @@ class MovieCarousel extends React.Component{
     <Carousel.Item>
           <div className="tint"></div>
 
-      <img width={900} height={500} alt="900x500" src="http://cdn.history.com/sites/2/2015/05/hith-titanic-tombstone-E.jpeg"/>
+      <img width={900} height={500} alt="900x500" src="https://s3.amazonaws.com/actionmovie/a4.jpg"/>
       <Carousel.Caption>
         <Link to="movie">
-          <Button bsStyle="info">Watch Now</Button>
+          <Button bsStyle="info" onClick={this.saveCurMovieInfo2}>Watch Now</Button>
         </Link>
-        <h3>Second slide label</h3>
+        <h3>Olympus Has Fallen (2013)</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </Carousel.Caption>
     </Carousel.Item>
@@ -43,12 +81,12 @@ class MovieCarousel extends React.Component{
     <Carousel.Item>
           <div className="tint"></div>
 
-      <img width={900} height={500} alt="900x500" src="http://cdn.history.com/sites/2/2015/05/hith-titanic-tombstone-E.jpeg"/>
+      <img width={900} height={500} alt="900x500" src="https://s3.amazonaws.com/movielily/w2.jpg"/>
       <Carousel.Caption>
         <Link to="movie">        
-        <Button bsStyle="info">Watch Now</Button>
+        <Button bsStyle="info" onClick={this.saveCurMovieInfo3}>Watch Now</Button>
         </Link>
-        <h3>Third slide label</h3>
+        <h3>Enemy at the Gates (2001)</h3>
         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
       </Carousel.Caption>
     </Carousel.Item>
