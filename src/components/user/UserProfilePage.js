18c1cdccf9c527b501d682fed82d12a2e8da7a5b
mirandazhjm <Jingmei@dyn-160-39-192-64.dyn.columbia.edu>
2016-12-19 15:24:25 -0500

    profile

--- src/components/user/UserProfilePage.js
+++ src/components/user/UserProfilePage.js
@@ -6,34 +6,72 @@ class UserProfilePage extends React.Component {
 	render(){
 		return (
 			<div className="user-profile">
-				<h1>Manage Profile</h1>
-				<div className="col-md-3">
-					<img width="200px" height="150px" src="https://s3-us-west-2.amazonaws.com/imovie/harleyquinnposter.png"/>
-				</div>
-				<div className="col-md-9">
-				<div className="col-md-3">
-				<p>
-					User Name:	
-					</p>
-				</div>
-					<div className="col-md-3">
-					<span>
-						Gender:
-					</span>
-					</div>
-					<div className="col-md-3">
-				<p>
-					Marital status:	
-					</p>
-				</div>
-				</div>
-				<div className="row-md-4">
-				<Link to='/movies'>        
-	        		<Button bsStyle="info">Home</Button>
-        			</Link>
-				</div>
+			    <h1>Manage Profile</h1>
+			    <div className="col-md-3">
+			    	<div class="row">
+			            <img width="200px" height="150px" src="https://s3-us-west-2.amazonaws.com/imovie/harleyquinnposter.png"/>
+			        </div>
+			        <div className="row">
+			        	<br></br>
+			        	<Link to='/movies'>
+			        	<Button bsStyle="info">Home</Button>
+			        	</Link>
+			    	</div>
+			    </div>
+			    <div className="col-md-9">
+			        <h2>Personal information</h2>
+			        <div className="col-md-3">
+			            <p>
+			                User Name:
+			            </p>
+			        </div>
+			        <div className="col-md-3">
+							<p>
+							Gender:
+							</p>
+			        </div>
+			        <div className="col-md-4">
+			            <p>
+			                Marital status:
+			            </p>
+			        </div>
+			        <br></br>
+			        <div className="col-md-3">
+			            <p>
+			                Country:
+			            </p>
+			        </div>
+			        <div className="col-md-3">
+							<p>
+							State:
+							</p>
+			        </div>
+			        <div className="col-md-4">
+			            <p>
+			                City:
+			            </p>
+			        </div>
+			    </div>
+			    <div className="col-md-9">
+			        <h2>Personal interests</h2>
+			        <div className="col-md-3">
+			            <p>
+			                Comedy
+			            </p>
+			        </div>
+			        <div className="col-md-3">
+							<p>
+							Romance
+							</p>
+			        </div>
+			        <div className="col-md-3">
+			            <p>
+			                Mystery
+			            </p>
+			        </div>
+			    </div>
 			</div>
-		);
+			);
 	}
 }
 
