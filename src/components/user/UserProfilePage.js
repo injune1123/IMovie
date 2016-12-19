646866638de73fbb70ceb85a833c7622ceb7aec0
mirandazhjm <Jingmei@dyn-160-39-192-64.dyn.columbia.edu>
2016-12-19 16:22:12 -0500

    profile

--- src/components/user/UserProfilePage.js
+++ src/components/user/UserProfilePage.js
@@ -21,19 +21,13 @@ class UserProfilePage extends React.Component {
 			    <div className="col-md-9">
 			        <h2>Personal information</h2>
 			        <div className="col-md-3">
-			            <p>
-			                User Name:
-			            </p>
+			            <p>User Name:</p>
 			        </div>
 			        <div className="col-md-3">
-							<p>
-							Gender:
-							</p>
+							<p>Gender:</p>
 			        </div>
 			        <div className="col-md-4">
-			            <p>
-			                Marital status:
-			            </p>
+			            <p>Marital status:</p>
 			        </div>
 			        <br></br>
 			        <div className="col-md-3">
@@ -52,22 +46,33 @@ class UserProfilePage extends React.Component {
 			            </p>
 			        </div>
 			    </div>
+			    <br></br>
 			    <div className="col-md-9">
 			        <h2>Personal interests</h2>
 			        <div className="col-md-3">
-			            <p>
-			                Comedy
-			            </p>
+			            <p>Comedy</p>
+			            <input type="range" min="0" max="50" value="10" />
 			        </div>
 			        <div className="col-md-3">
-							<p>
-							Romance
-							</p>
+						<p>Romance</p>
+						<input type="range" min="0" max="50" value="20" />
+			        </div>
+			        <div className="col-md-4">
+			            <p>Mystery</p>
+			            <input type="range" min="0" max="50" value="45" />
+			        </div>
+			        <br></br>
+			        <div className="col-md-3">
+			            <p>Animation</p>
+			            <input type="range" min="0" max="50" value="10" />
 			        </div>
 			        <div className="col-md-3">
-			            <p>
-			                Mystery
-			            </p>
+						<p>Drama</p>
+						<input type="range" min="0" max="50" value="20" />
+			        </div>
+			        <div className="col-md-4">
+			            <p>Crime</p>
+			            <input type="range" min="0" max="50" value="45" />
 			        </div>
 			    </div>
 			</div>
